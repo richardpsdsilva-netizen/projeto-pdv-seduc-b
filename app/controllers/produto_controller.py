3af357fe1ec3c0fb6a8f69c03f47d09dce49e8b0
Richard <richardpsdsilva@gmail.com>
2026-08-26 14:25:34 -0300

    feat - adicionando paginação no html

--- app/controllers/produto_controller.py
+++ app/controllers/produto_controller.py
@@ -2,6 +2,7 @@
 
 import os
 import shutil
+import math
 from fastapi import APIRouter, Depends, Request, Form, UploadFile, File, status
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
@@ -43,7 +44,20 @@ def listar_produtos(
     if categoria_id:
         query = query.filter(Produto.categoria_id == categoria_id)
 
-    produtos    = query.order_by(Produto.nome).all()
+    query = query.order_by(Produto.nome)
+
+    total_produtos = query.count()
+
+    pagina = max(pagina , 1 )
+    por_pagina = max (por_pagina , 1 )
+
+    total_paginas = math.ceil(total_produtos / por_pagina ) if total_produtos else 1
+
+    offset = ( pagina -1 ) * por_pagina
+
+    produtos = query.offset(offset).limit(por_pagina).all
+
+
     categorias  = db.query(Categoria).filter(Categoria.ativo == True).all()
 
     return templates.TemplateResponse(
@@ -56,6 +70,10 @@ def listar_produtos(
             "categorias":   categorias,
             "busca":        busca,
             "categoria_id": categoria_id,
+            "pagina":       pagina,
+            "por_pagina":   por_pagina,
+        "total_produtos":   total_produtos,
+        "total_paginas":    total_paginas
         }
     )
 
